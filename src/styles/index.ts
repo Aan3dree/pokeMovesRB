16ccf6f5dbe3b171413cd4647e224ad1561a482a
André Luiz <an3dree@gmail.com>
2021-08-24 11:36:51 -0300

    first commit

---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1F1D45',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: '#fff'
+    },
+    header: {
+        
+    }
+  });
+
+export default styles;
