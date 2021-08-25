4f3e5c937404451fba679fb517e438046097ba97
André Luiz <an3dree@gmail.com>
2021-08-25 12:24:43 -0300

    second commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, View, TextInput, Image, ScrollView, FlatList } from 'react-native';
+import styles from './src/styles';
+import axios from 'axios';
+
 
 export default function App() {
+  
+  const [moves, setMoves] = useState<string>("")
+  const [name, setName] = useState<string>()
+
+  function handleName(value: string) {
+    setName(value);
+    console.log(name)
+  }
+  
+
+  function handleMoves() {
+    
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name?.toLowerCase()}`, {
+      params:{
+        _moves: moves
+      }
+    })
+      .then(res => {
+        const generation: string = res.data.moves[0].version_group_details[0].version_group.name;
+        console.log(generation);
+        setMoves(res.data.result);
+        console.log(res)
+      })
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+      <View style={styles.container}>
+        <View style={styles.logoWraper}>
+          <Image 
+            style={styles.logo}
+            source={require('./src/assets/Img/logo.png')}
+          /> 
+        </View>
+        <View style={styles.inputWraper}>
+          <TextInput
+            style={styles.input} 
+            placeholder={'Nome do Pokemon'}
+            //value={name}
+            onChangeText={handleName}
+          />
+          <Button
+            title="Buscar"
+            onPress={() => handleMoves()}
+            color="blue"
+          />
+        </View>
+        <ScrollView>
+          <FlatList>
+            {}
+          </FlatList>
+        </ScrollView>
+        <View style={styles.embaixo}>
+          
+        </View> 
+        <StatusBar style="auto" />
+      </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,13 +3,42 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexDirection: 'column',
       backgroundColor: '#1F1D45',
       alignItems: 'center',
       justifyContent: 'center',
       color: '#fff'
     },
-    header: {
-        
+    logoWraper:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 80,
+      flex: 1
+    },
+    logo:{
+      height: 124.4,
+      width: 320.4,
+      
+    },
+    inputWraper: {
+      marginTop: 40,
+      flex: 2,
+      width: '60%',
+    },
+    input:{
+      color: '#1F1D45',
+      backgroundColor: '#c6c6c6',
+      borderBottomColor: '#000000',
+      borderBottomWidth: 1,
+      borderRadius: 6,
+      marginBottom: 15,
+      height: 42,
+      textAlign: 'center'
+    },
+    embaixo:{
+      flex: 3,
+      backgroundColor: "green"
+      
     }
   });
 
