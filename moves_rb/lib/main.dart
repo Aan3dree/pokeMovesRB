df0167f59d01fd6a2857c96c1c5c4459a2c070b2
André Luiz <an3dree@gmail.com>
2022-05-27 16:00:15 -0300

    removing the widget to separate file

--- moves_rb/lib/main.dart
+++ moves_rb/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:moves_rb/utils/http.dart';
-
+import './screens/home.dart';
 import 'models/poke.dart';
 
 void main() {
@@ -16,48 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ShowPokemon(),
-    );
-  }
-}
-
-class ShowPokemon extends StatefulWidget {
-  const ShowPokemon({Key? key}) : super(key: key);
-
-  @override
-  State<ShowPokemon> createState() => _ShowPokemonState();
-}
-
-class _ShowPokemonState extends State<ShowPokemon> {
-  HttpHelper http = HttpHelper();
-  String pokeName = '';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Pokes'),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            onChanged: (e) {
-              print(e);
-              setState(() {
-                pokeName = e;
-              });
-            },
-          ),
-          TextButton(
-            onPressed: () {
-              http.getPokemon(pokeName);
-            },
-            child: Text('Fetch'),
-          ),
-        ],
-      ),
+      theme: ThemeData.dark(),
+      home: const Home(),
     );
   }
 }
