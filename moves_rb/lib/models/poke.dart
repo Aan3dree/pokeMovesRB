9567736c2b4248e1dcc8964f68a58e1e80acdfff
André Luiz <an3dree@gmail.com>
2022-05-27 16:00:48 -0300

    propertie sprite url to show the pokemon sprite

--- moves_rb/lib/models/poke.dart
+++ moves_rb/lib/models/poke.dart
@@ -1,7 +1,8 @@
 class Pokemon {
   String name = '';
   List<String> moves = [];
+  String sprite = '';
 
 //const generation: string = res.data.moves[0].version_group_details[0].version_group.name;
-  Pokemon(this.name, this.moves);
+  Pokemon(this.name, this.moves, this.sprite);
 }
