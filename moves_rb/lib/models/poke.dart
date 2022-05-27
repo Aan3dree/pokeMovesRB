33abd3840348d3e0d080b6ce5cad64ac63795bd0
André Luiz <an3dree@gmail.com>
2022-05-26 23:02:15 -0300

    refactor class

---
+++ moves_rb/lib/models/poke.dart
@@ -0,0 +1,7 @@
+class Pokemon {
+  String name = '';
+  List<String> moves = [];
+
+//const generation: string = res.data.moves[0].version_group_details[0].version_group.name;
+  Pokemon(this.name, this.moves);
+}
